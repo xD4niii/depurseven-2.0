b57431deeea9ec26bcd44e52b81623b0f3504699
Daniele Pizzarelli <danielepizz31@gmail.com>
2024-09-07 00:15:09 +0200

    Fix home page, upgrade faq, aggiunto script per faq

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,36 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+  const checkboxes = document.querySelectorAll('.faq_checkbox');
+
+  checkboxes.forEach(checkbox => {
+    checkbox.addEventListener('change', (event) => {
+      // Se il checkbox è selezionato, deseleziona gli altri
+      if (event.target.checked) {
+        checkboxes.forEach(cb => {
+          if (cb !== event.target) {
+            cb.checked = false;
+          }
+        });
+      }
+    });
+  });
+
+
+
 //Funzione Tasto copia
 
-const copyButtons = document.querySelectorAll('.copy-btn');
+  const copyButtons = document.querySelectorAll('.copy-btn');
 
-copyButtons.forEach(button => {
-  button.addEventListener('click', function() {
+  copyButtons.forEach(button => {
+    button.addEventListener('click', function() {
 
-    const textToCopy = this.getAttribute('data-number');
+      const textToCopy = this.getAttribute('data-number');
 
-    navigator.clipboard.writeText(textToCopy).then(function() {
-      alert('Numero copiato: ' + textToCopy);
-    }).catch(function(err) {
-      console.error('Errore durante la copia: ', err);
+      navigator.clipboard.writeText(textToCopy).then(function() {
+        alert('Numero copiato: ' + textToCopy);
+      }).catch(function(err) {
+        console.error('Errore durante la copia: ', err);
+      });
     });
   });
 });
